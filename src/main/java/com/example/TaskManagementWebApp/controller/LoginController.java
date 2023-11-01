357fbaf140c55f04f06d0baa07cc68eaa26c9227
ottotto1178 <ottotto1178@gmail.com>
2023-11-01 16:26:46 +0900

    First Commit

---
+++ src/main/java/com/example/TaskManagementWebApp/controller/LoginController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.TaskManagementWebApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author ottotto
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @GetMapping
+    public String view(){
+        return "login";
+    }
+}
